99bcdcd255e08c19b5543a773fee15e78e53c813
Mansi Gupta <mansigupta5858@gmail.com>
2026-03-01 18:10:13 -0800

    Spring initializer for Backend

---
+++ node_backend_app/eventServices/src/main/java/com/node/eventServices/EventServicesApplication.java
@@ -0,0 +1,13 @@
+package com.node.eventServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventServicesApplication.class, args);
+	}
+
+}
