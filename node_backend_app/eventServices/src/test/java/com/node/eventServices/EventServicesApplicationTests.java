1a38c0969ff78923b59fcff226007ffe57adb46d
Mansi Gupta <mansigupta5858@gmail.com>
2026-03-01 18:10:13 -0800

    Spring initializer for Backend

---
+++ node_backend_app/eventServices/src/test/java/com/node/eventServices/EventServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.node.eventServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
